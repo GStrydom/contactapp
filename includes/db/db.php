e7832565971f17f664e3241fc89bd3ff140e3eb9
Gregory <gregory.strydom079@gmail.com>
2020-12-05 13:55:20 +0200

    Basic db setup

---
+++ includes/db/db.php
@@ -0,0 +1,8 @@
+<?php
+    define("HOST", "localhost");
+    define("USER", "root");
+    define("PASSWORD", "");
+    define("PORT", "3306");
+    define("DBNAME", "contactappdb");
+
+
--- index.php
+++ index.php
@@ -2,19 +2,36 @@
     session_start();
 
     if($_SESSION['loggedIn'] == true) {
-        $linkText = "Login";
-        $linkUrl = "includes/login.php";
-    } else {
         $linkText = "Logout";
         $linkUrl = "includes/logout.php";
+    } else {
+        $linkText = "Login";
+        $linkUrl = "includes/login.php";
     }
 
+    $rows = [];
+
     require('includes/header.php');
-
-    require('includes/footer.php');
 ?>
-
     <div class="head-bar">
         <h1>Contact App</h1>
         <a href=""><?php echo $linkText; ?></a>
     </div>
+
+    <div class="content">
+        <table>
+            <tr>
+                <th>Name</th>
+                <th></th>
+                <th></th>
+            </tr>
+            <?php
+                foreach($rows as $row) {
+                    echo "<tr><td>$row->name</td><td>Edit</td><td>Delete</td></tr>";
+                }
+            ?>
+        </table>
+    </div>
+
+    <?php require('includes/footer.php'); ?>
+
