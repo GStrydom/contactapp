8bad7785e310c149c58999c51f71e7d93b0c74a6
Gregory <gregory.strydom079@gmail.com>
2020-12-05 14:29:48 +0200

    Updates

--- includes/db/db.php
+++ includes/db/db.php
@@ -5,4 +5,16 @@
     define("PORT", "3306");
     define("DBNAME", "contactappdb");
 
+    $dsn = "mysql:host=" . HOST . ";dbname=" . DBNAME;
+
+    try {
+        $pdo = new PDO($dsn, USER, PASSWORD);
+    } catch(PDOException $e) {
+        die("There was an error, " . $e);
+    }
+
+    function login($email, $password, $pdo) {
+        $sql = "SELECT * FROM users WHERE email=:email and password=:password";
+        $pdo->prepare($sql);
+    }
 
---
+++ includes/registration/logout.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cheryl
+ * Date: 12/5/2020
+ * Time: 1:57 PM
+ */
