07a97ea4f4b40dc22744c2131694409e53f4f5ae
Gregory <gregory.strydom079@gmail.com>
2020-12-05 14:49:07 +0200

    Updates

--- includes/db/db.php
+++ includes/db/db.php
@@ -5,16 +5,33 @@
     define("PORT", "3306");
     define("DBNAME", "contactappdb");
 
+    // DSN settings
     $dsn = "mysql:host=" . HOST . ";dbname=" . DBNAME;
 
+    // Make sure we can connect to the database
     try {
         $pdo = new PDO($dsn, USER, PASSWORD);
+        $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
     } catch(PDOException $e) {
         die("There was an error, " . $e);
     }
 
+    function getList() {
+
+    }
+
+    // Login the user
     function login($email, $password, $pdo) {
         $sql = "SELECT * FROM users WHERE email=:email and password=:password";
-        $pdo->prepare($sql);
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute([$email, $password]);
+        $user = $stmt->fetch();
+        if($user) {
+
+        }
+    }
+
+    function logout() {
+        session_destroy();
     }
 
